e1f32705360bd83b4ca8b81ef0b812fe8f6f9591
Rüdiger Oertel <ro@suse.de>
2009-05-22 16:56:48 +0100

    - more ugly tweaks to uname hack

--- helper/uname.sh
+++ helper/uname.sh
@@ -10,6 +10,14 @@ if test -z "$MREL" -a -L /usr/src/linux -a -d /usr/src/linux ; then
     MREL=$( shopt -s nullglob; set -- /lib/modules/*-default /lib/modules/* ; basename $1 )
 fi
 
+if test -z "$MREL" -a -L /usr/src/linux -a -d /usr/src/linux -a -d /usr/src/linux-obj ; then
+    MVER=$(readlink /usr/src/linux)
+    MVER=${MVER#linux-}
+    ARCH=$(uname.bin -m)
+    MFLAV=$( shopt -s nullglob; set -- /usr/src/linux-obj/$ARCH/default /usr/src/linux-obj/$ARCH/* ; test -n "$1" && basename $1 )
+    test -n "$MVER" -a -n "$MFLAV" && MREL=$MVER-$MFLAV
+fi
+
 if test -z "$MREL" -a -f /usr/src/linux/Makefile ; then
     MREL=`grep "^VERSION = " /usr/src/linux/Makefile 2> /dev/null | sed -e "s/VERSION = //"`
     MREL=$MREL.`grep "^PATCHLEVEL = " /usr/src/linux/Makefile 2> /dev/null | sed -e "s/PATCHLEVEL = //"`
